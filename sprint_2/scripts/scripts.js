9dcf343f7d3116bf0eb63d925cd793998cec73df
Nastya <anastasion0107@gmail.com>
2024-10-27 02:42:48 +0700

    сделала получение файла из формы

--- sprint_2/scripts/scripts.js
+++ sprint_2/scripts/scripts.js
@@ -1,3 +1,41 @@
+const formName = document.getElementById('nameForm')
+const form = document.getElementById('formCreate')
+
+let createForm = (file) => {
+    formName.innerHTML = file.name
+    form.innerHTML = ''
+    file.fields.forEach(item => {
+        let div = document.createElement('div')
+        let input = document.createElement('input')
+        if(item.label){
+            let label = document.createElement('label')
+            label.innerHTML = item.label
+            div.append(label)
+        }
+        if(item.input.required){
+            input.setAttribute('required', item.input.required)
+        }
+        if(item.input.type === 'technology'){
+            input = document.createElement('select')
+            item.input.technologies.forEach(tech => {
+                let option = document.createElement('option')
+                option.innerHTML = tech
+                input.append(option)
+            })
+        }
+
+        if(item.input.placeholder){
+            input.setAttribute('placeholder', item.input.placeholder)
+        }
+        if(item.input.type !== 'technology'){
+            input.setAttribute('type', item.input.type)
+        }
+
+        div.append(input)
+        form.append(div)
+    })
+}
+
 function readFile(input) {
     let file = input.files[0];
 
@@ -6,7 +44,7 @@ function readFile(input) {
     reader.readAsText(file);
 
     reader.onload = function() {
-        console.log(JSON.parse(String(reader.result)))
+        createForm(JSON.parse(String(reader.result)))
     };
 
     reader.onerror = function() {
@@ -14,3 +52,5 @@ function readFile(input) {
     };
 
 }
+
+

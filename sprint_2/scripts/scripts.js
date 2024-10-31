bec752a73481871eccb4089dded38f83d13a2cd6
Nastya <anastasion0107@gmail.com>
2024-10-31 13:58:22 +0700

    закончено

--- sprint_2/scripts/scripts.js
+++ sprint_2/scripts/scripts.js
@@ -2,11 +2,20 @@ const formName = document.getElementById('nameForm')
 const form = document.getElementById('formCreate')
 const btn = document.getElementById('btn')
 
-let resetForm = () => {
-    form.innerHTML = ''
+btn.onclick = e => {
+    resetForm(e)
+}
+
+let resetForm = (event) => {
+    event.preventDefault()
+    form.innerHTML = '';
+    let file = document.getElementById('file-selector');
+    file.value = null;
+    formName.innerHTML = '';
 }
 
 let createForm = (file) => {
+
     formName.innerHTML = file.name
     form.innerHTML = ''
     btn.classList.remove('d-none')
@@ -16,36 +25,19 @@ let createForm = (file) => {
 
         let input = document.createElement('input')
         input.classList.add('form-control')
-        if(item.label){
-            let label = document.createElement('label')
-            label.innerHTML = item.label
-            div.append(label)
-            label.classList.add('form-label')
+
+        function createLabel(){
+            if(item.label){
+                let label = document.createElement('label')
+                label.innerHTML = item.label
+                div.append(label)
+                label.classList.add('form-label')
+            }
         }
-        if(item.input.required && item.input['required:']) input.setAttribute('required', item.input.required)
 
-        if(item.input.type === 'technology'){
-            input = document.createElement('select')
-            input.classList.add('form-select')
-            item.input.technologies.forEach(tech => {
-                let option = document.createElement('option')
-                option.innerHTML = tech
-                input.append(option)
-            })
-        }
-        if(item.input.type === 'textarea'){
-            input = document.createElement('textarea')
-            input.classList.add('form-control')
-        }
-        if(item.input.type === 'checkbox') input.classList.add('form-check-input')
+        createLabel()
 
-        if(item.input.multiple) input.setAttribute('multiple', item.input.multiple)
-
-        if(item.input.placeholder) input.setAttribute('placeholder', item.input.placeholder)
-
-        if(item.input.type !== 'technology') input.setAttribute('type', item.input.type)
-
-        if(item.input.type === 'color'){
+        function createDataList(){
             let datalist = document.createElement('datalist')
             datalist.setAttribute('id', 'colors')
             input.setAttribute('list', 'colors')
@@ -56,8 +48,45 @@ let createForm = (file) => {
                 option.setAttribute('value', color)
                 datalist.append(option)
             })
-            div.append(datalist)
+            div.append(datalist);
         }
+
+        switch (item.input.type) {
+            case 'technology':
+                input = document.createElement('select')
+                input.classList.add('form-select')
+                item.input.technologies.forEach(tech => {
+                    let option = document.createElement('option')
+                    option.innerHTML = tech
+                    input.append(option)
+                });
+                break;
+
+            case 'checkbox':
+                input.classList.add('form-check-input');
+                break;
+
+            case 'textarea':
+                input = document.createElement('textarea')
+                input.classList.add('form-control');
+                break;
+
+            case 'color':
+                createDataList()
+                break;
+            default:
+                input.setAttribute('type', item.input.type)
+
+        }
+
+        if(item.input.required || item.input['required:'])
+            input.setAttribute('required', 'true');
+
+        if(item.input.multiple)
+            input.setAttribute('multiple', item.input.multiple)
+
+        if(item.input.placeholder)
+            input.setAttribute('placeholder', item.input.placeholder)
 
         if(item.input.filetype){
             let newFileType=  item.input.filetype.map(filetype => {
@@ -65,6 +94,7 @@ let createForm = (file) => {
             })
             input.setAttribute('accept', newFileType.join(', '))
         }
+
 
         div.append(input)
         form.append(div)
@@ -97,6 +127,7 @@ let createForm = (file) => {
             form.append(div)
         })
     }
+
     if(file.buttons){
         let div = document.createElement('div')
         file.buttons.forEach(button => {
@@ -113,6 +144,7 @@ let createForm = (file) => {
 
 function readFile(input) {
     let file = input.files[0];
+    if (!file) return;
 
     let reader = new FileReader();
 
@@ -127,5 +159,7 @@ function readFile(input) {
     };
 
 }
+
+
 
 
